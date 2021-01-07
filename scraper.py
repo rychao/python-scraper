4d9d5a32c256718cf9b11bdd5b1e0ba18f247f6d
rychao <rychao@gmail.com>
2021-01-07 03:06:21 -0800

    simplified xpath click w/ size

--- scraper.py
+++ scraper.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-#import chromedriver_binary  # Adds chromedriver binary to path
 from selenium.webdriver.common.keys import Keys
-
 import json
 import string
 
 class Scraper(object):
-
     global driver # webdriver crashes, changing to global seems to fix
     driver = webdriver.Chrome()
 
@@ -26,11 +23,10 @@ class Scraper(object):
         self.ccv = ccv
 
     def scrape_init(self):
-
-        #driver = webdriver.Chrome(executable_path=r"/usr/local/bin/chromedriver")
+        #kith US SHOE: https://kith.com/collections/mens-footwear/products/vn0a4uud1mk
+        #kith gloves: https://kith.com/collections/kith/products/kh9588-101
         url = self.url
         size = self.size
-        #size = input("Size: ")
         print("Requested Size: ", size)
         driver.get(url)
 
@@ -52,95 +48,19 @@ class Scraper(object):
         cardExp1 = cardExp[1]
         ccv = self.ccv
 
-        # CLOTHING SIZE
-        if size == 'S':
-            driver.find_element_by_xpath('//div[@data-value="S" and @class="swatch-element s"]').click()
-        elif size == 'M':
-            driver.find_element_by_xpath('//div[@data-value="M" and @class="swatch-element m"]').click()
-        elif size == 'L':
-            driver.find_element_by_xpath('//div[@data-value="L" and @class="swatch-element l"]').click()
-        elif size == 'XL':
-            driver.find_element_by_xpath('//div[@data-value="XL" and @class="swatch-element xl"]').click()
+        if size == 'S' or size == 'M' or size == 'L' or size == "XL":         # CLOTHING SIZE (missing XS)
+            driver.find_element_by_xpath('//div[@data-value="{}" and @class="swatch-element {}"]'.format(size, size.lower())).click()
 
-        # SHOE SIZES (Mens US 3-15, EU 36-46)
-        # ---US SHOE SIZE---
-        if size == '3':
-            driver.find_element_by_xpath('//div[@data-value="3" and @class="swatch-element 3"]').click()
-        elif size == '4':
-            driver.find_element_by_xpath('//div[@data-value="4" and @class="swatch-element 4"]').click()
-        elif size == '4.5':
-            driver.find_element_by_xpath('//div[@data-value="4.5" and @class="swatch-element 4-5"]').click()
-        elif size == '5':
-            driver.find_element_by_xpath('//div[@data-value="5" and @class="swatch-element 5"]').click()
-        elif size == '5.5':
-            driver.find_element_by_xpath('//div[@data-value="5.5" and @class="swatch-element 5-5"]').click()
-        elif size == '6':
-            driver.find_element_by_xpath('//div[@data-value="6" and @class="swatch-element 6"]').click()
-        elif size == '6.5':
-            driver.find_element_by_xpath('//div[@data-value="6.5" and @class="swatch-element 6-5"]').click()
-        elif size == '7':
-            driver.find_element_by_xpath('//div[@data-value="7" and @class="swatch-element 7"]').click()
-        elif size == '7.5':
-            driver.find_element_by_xpath('//div[@data-value="7.5" and @class="swatch-element 7-5"]').click()
-        elif size == '8':
-            driver.find_element_by_xpath('//div[@data-value="8" and @class="swatch-element 8"]').click()
-        elif size == '8.5':
-            driver.find_element_by_xpath('//div[@data-value="8.5" and @class="swatch-element 8-5"]').click()
-        elif size == '9':
-            driver.find_element_by_xpath('//div[@data-value="9" and @class="swatch-element 9"]').click()
-        elif size == '9.5':
-            driver.find_element_by_xpath('//div[@data-value="9.5" and @class="swatch-element 9-5"]').click()
-        elif size == '10':
-            driver.find_element_by_xpath('//div[@data-value="10" and @class="swatch-element 10"]').click()
-        elif size == '10.5':
-            driver.find_element_by_xpath('//div[@data-value="10.5" and @class="swatch-element 10-5"]').click()
-        elif size == '11':
-            driver.find_element_by_xpath('//div[@data-value="11" and @class="swatch-element 11"]').click()
-        elif size == '11.5':
-            driver.find_element_by_xpath('//div[@data-value="11.5" and @class="swatch-element 11-5"]').click()
-        elif size == '12':
-            driver.find_element_by_xpath('//div[@data-value="12" and @class="swatch-element 12"]').click()
-        elif size == '12.5':
-            driver.find_element_by_xpath('//div[@data-value="12.5" and @class="swatch-element 12-5"]').click()
-        elif size == '13':
-            driver.find_element_by_xpath('//div[@data-value="13" and @class="swatch-element 13"]').click()
-        elif size == '14':
-            driver.find_element_by_xpath('//div[@data-value="14" and @class="swatch-element 14"]').click()
-        elif size == '15':
-            driver.find_element_by_xpath('//div[@data-value="15" and @class="swatch-element 15"]').click()
-        elif size == '35':    # ---EU SHOE SIZE---
-            driver.find_element_by_xpath('//div[@data-value="35" and @class="swatch-element 35"]').click()
-        elif size == '36':
-            driver.find_element_by_xpath('//div[@data-value="36" and @class="swatch-element 36"]').click()
-        elif size == '37':
-            driver.find_element_by_xpath('//div[@data-value="37" and @class="swatch-element 37"]').click()
-        elif size == '38':
-            driver.find_element_by_xpath('//div[@data-value="38" and @class="swatch-element 38"]').click()
-        elif size == '39':
-            driver.find_element_by_xpath('//div[@data-value="39" and @class="swatch-element 39"]').click()
-        elif size == '40':
-            driver.find_element_by_xpath('//div[@data-value="40" and @class="swatch-element 40"]').click()
-        elif size == '41':
-            driver.find_element_by_xpath('//div[@data-value="41" and @class="swatch-element 41"]').click()
-        elif size == '42':
-            driver.find_element_by_xpath('//div[@data-value="42" and @class="swatch-element 42"]').click()
-        elif size == '43':
-            driver.find_element_by_xpath('//div[@data-value="43" and @class="swatch-element 43"]').click()
-        elif size == '44':
-            driver.find_element_by_xpath('//div[@data-value="44" and @class="swatch-element 44"]').click()
-        elif size == '45':
-            driver.find_element_by_xpath('//div[@data-value="45" and @class="swatch-element 45"]').click()
-        elif size == '46':
-            driver.find_element_by_xpath('//div[@data-value="46" and @class="swatch-element 46"]').click()
-
+        if '.5' in size:         # SHOE SIZES (Mens US 3-15, EU 36-46)
+            driver.find_element_by_xpath('//div[@data-value="{}" and @class="swatch-element {}"]'.format(size, size.replace(".5", "-5"))).click()
+        else:
+            driver.find_element_by_xpath('//div[@data-value="{}" and @class="swatch-element {}"]'.format(size, size)).click()
 
         driver.find_element_by_name('add').click()
-        #Takes a moment to add to cart, buffer for 5sec
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(5) # wait for cart button
         driver.find_element_by_name('checkout').click()
-        driver.implicitly_wait(60) #waits up to 60s if queue
+        driver.implicitly_wait(60) # wait 1 min in case of QUEUE
 
-        #emailInput = driver.find_element_by_xpath('//*[@id="checkout_email"]')
         emailInput = driver.find_element_by_id('checkout_email')
         emailInput.send_keys(email)
         driver.find_element_by_id('checkout_buyer_accepts_marketing').click()
@@ -193,7 +113,6 @@ def main():
     cardName = (elements['card name'])
     cardExp = (elements['card expiry'])
     ccv = (elements['ccv'])
-    #print(phone)
 
     test = Scraper(url, size, email, firstName, lastName, address, city, zip, phone, cardNum, cardName, cardExp, ccv)
     test.scrape_init()
